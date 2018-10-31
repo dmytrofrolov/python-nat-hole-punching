42ae9a5d8a7db61e997be189a137797f457dab5d
Dmytro Frolov <dmytro.fr@gmail.com>
2018-10-31 14:20:36 +0200

    Added connection abstraction

---
+++ connection.py
@@ -0,0 +1,60 @@
+import json
+import time
+
+class Connection():
+    ip = ""
+    port = 3333
+    connectionTimestamp = 0
+
+    def __init__(self, addrTuple, connectionTimestamp):
+        self.ip, self.port = addrTuple
+        self.connectionTimestamp = connectionTimestamp
+
+    def getAddress(self):
+        return self.ip, self.port
+
+    def getConnectionTimestamp(self):
+        return self.connectionTimestamp
+
+    def setConnectionTimestamp(self, newConnectionTimestamp):
+        self.connectionTimestamp = newConnectionTimestamp
+
+    def isOutdatedConnection(self):
+        return self.getConnectionTimestamp() < time.time() - 60
+
+    def getAsDict(self):
+        return {
+            "ip" : self.ip,
+            "port" : self.port,
+            "lastConnection" : self.connectionTimestamp
+        }
+
+
+class ConnectionsList():
+    __list = []
+
+    def addToList(self, connection):
+        if self.isConnectionInList(connection):
+            self.removeFromList(connection)
+        self.__list.append(connection)
+
+    def removeFromList(self, connection):
+        for index, c in enumerate(self.__list):
+            if c.getAddress() == connection.getAddress():
+                self.__list = self.__list[:index] + self.__list[index+1:]
+                return
+
+    def isConnectionInList(self, connection):
+        for c in self.__list:
+            if c.getAddress() == connection.getAddress():
+                return True
+        return False
+
+    def clearOutdatedItems(self):
+        for c in self.__list:
+            if c.isOutdatedConnection():
+                self.removeFromList(c)
+
+    def getListJson(self):
+        self.clearOutdatedItems()
+        return json.dumps(self.__list, default=lambda o: o.getAsDict(), ensure_ascii=False).encode()
--- udp_server.py
+++ udp_server.py
@@ -2,26 +2,29 @@ import logging
 import socket
 import sys
 from util import *
+from connection import *
+import time
 
 logger = logging.getLogger()
-addresses = []
-
+# addresses = []
+connections = ConnectionsList()
 
 def main(host='0.0.0.0', port=9999):
     sock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
+
+    host = '0.0.0.0'
     sock.bind((host, port))
     while True:
-        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        data, addr = sock.recvfrom(1024)
+
         logger.info("connection from: %s", addr)
-        addresses.append(addr)
-        if len(addresses) >= 2:
-            logger.info("server - send client info to: %s", addresses[0])
-            sock.sendto(addr_to_msg(addresses[1]), addresses[0])
-            logger.info("server - send client info to: %s", addresses[0])
-            sock.sendto(addr_to_msg(addresses[0]), addresses[1])
-            addresses.pop(1)
-            addresses.pop(0)
+
+        new_connection = Connection(addr, time.time())
+        connections.addToList(new_connection)
+
+        sock.sendto(connections.getListJson(), addr)
+
 
 
 if __name__ == '__main__':
